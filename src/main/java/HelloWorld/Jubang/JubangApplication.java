4a401ab6cd096e7a7f94d8f0cb7a551325c55095
sohyemin <shm8201@naver.com>
2025-09-10 22:26:52 +0900

    first commit

---
+++ src/main/java/HelloWorld/Jubang/JubangApplication.java
@@ -0,0 +1,13 @@
+package HelloWorld.Jubang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JubangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HelloWorld/Jubang/JubangApplicationTests.java
@@ -0,0 +1,13 @@
+package HelloWorld.Jubang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
