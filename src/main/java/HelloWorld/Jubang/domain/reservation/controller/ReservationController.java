ea6d6721d0447a96385b6d5adbfd8dd62ca6b0a7
sohyemin <shm8201@naver.com>
2025-10-30 23:43:01 +0900

    - 예약 관련 추가

---
+++ src/main/java/HelloWorld/Jubang/domain/reservation/controller/ReservationController.java
@@ -0,0 +1,4 @@
+package HelloWorld.Jubang.domain.reservation.controller;
+
+public class ReservationController {
+}
--- src/main/java/HelloWorld/Jubang/domain/reservation/entity/Reservation.java
+++ src/main/java/HelloWorld/Jubang/domain/reservation/entity/Reservation.java
@@ -1,4 +1,15 @@
 package HelloWorld.Jubang.domain.reservation.entity;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
 public class Reservation {
 }
