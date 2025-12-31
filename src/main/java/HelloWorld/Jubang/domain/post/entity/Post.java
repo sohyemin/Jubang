368c67928568c05dafc26b902fe6369412efa162
sohyemin <shm8201@naver.com>
2025-12-31 23:07:02 +0900

    - post 관련 추가

--- src/main/java/HelloWorld/Jubang/domain/post/entity/Post.java
+++ src/main/java/HelloWorld/Jubang/domain/post/entity/Post.java
@@ -1,9 +1,25 @@
 package HelloWorld.Jubang.domain.post.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import HelloWorld.Jubang.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "email", nullable = false)
+    private User user;
+
+    private String title;
+    private String content;
 }
